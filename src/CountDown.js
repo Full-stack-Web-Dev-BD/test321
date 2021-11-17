3d7e8bf4284458fc9835fe25f5f6d2bc396626fb
Alamin <alaminprogramerr@gmail.com>
2021-11-17 11:39:14 -0800

    v test

--- src/CountDown.js
+++ src/CountDown.js
@@ -1,47 +1,67 @@
-import React, { useEffect } from "react";
-import Social from "./component/Social";
-import Timer from "./component/Timer";
+// import React, { useEffect } from "react";
+// import Social from "./component/Social";
+// import Timer from "./component/Timer";
+// const CountDown = () => {
+//   useEffect(() => {
+//     var timerContent = document.getElementById('timer-content')
+//     timerContent.style.display = "none"
+//     var videoContent = document.getElementById('video-container')
+//     var video = document.getElementById('video')
+//     video.addEventListener('play', function () {
+//       setTimeout(() => {
+//         videoContent.style.display = "none"
+//         timerContent.style.display = "block"
+//         timerContent.classList.add('fade-in')
+//       }, 15000);
+//     })
+
+//   });
+//   return (
+//     <div className="App ">
+//       <div id="video-container" className="fullscreen-bg">
+//         <video
+//           loop
+//           autoPlay
+//           poster="img/videoframe.jpg"
+//           id="video"
+//           style={{ width: "100%" }}
+//           className="fullscreen-bg__video"
+//         >
+//           <source src="https://res.cloudinary.com/dq52qsb4j/video/upload/v1637127214/video_hvgx3s.mp4" type="video/mp4" />
+//         </video>
+//       </div>
+//       <div className=" counter-contant " id="timer-content">
+//         <div className="container">
+//           <h1>
+//             <img alt="img" style={{ width: '80%' }} src="/files/title.png" />
+//           </h1>
+//           <Timer />
+//           <Social />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CountDown;
+
+import React from 'react'
+
 const CountDown = () => {
-  useEffect(() => {
-    var timerContent = document.getElementById('timer-content')
-    timerContent.style.display = "none"
-    var videoContent = document.getElementById('video-container')
-    var video = document.getElementById('video')
-    video.addEventListener('play', function () {
-      setTimeout(() => {
-        videoContent.style.display = "none"
-        timerContent.style.display = "block"
-        timerContent.classList.add('fade-in')
-      }, 15000);
-    })
-
-  });
   return (
-    <div className="App ">
-      <div id="video-container" className="fullscreen-bg">
-        <video
-          loop
-          autoPlay
-          poster="img/videoframe.jpg"
-          id="video"
-          style={{ width: "100%" }}
-          className="fullscreen-bg__video"
-        >
-          <source src="https://res.cloudinary.com/dq52qsb4j/video/upload/v1637127214/video_hvgx3s.mp4" type="video/mp4" />
-        </video>
-      </div>
-      <div className=" counter-contant " id="timer-content">
-        <div className="container">
-          <h1>
-            <img alt="img" style={{ width: '80%' }} src="/files/title.png" />
-          </h1>
-          <Timer />
-          <Social />
-        </div>
-      </div>
+    <div>
+      <video
+        loop
+        autoPlay
+        poster="img/videoframe.jpg"
+        id="video"
+        style={{ width: "100%" }}
+        className="fullscreen-bg__video"
+      >
+        <source src="https://res.cloudinary.com/dq52qsb4j/video/upload/v1637127214/video_hvgx3s.mp4" type="video/mp4" />
+      </video>
     </div>
-  );
-};
+  )
+}
 
-export default CountDown;
-// db:mongodb+srv://admin:<admin>@cluster0.hbf5v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+export default CountDown
